1032a7b98a1f773a6f439712a7970f8479e8236d
Kresta01 <mata.zaplatilek@gmail.com>
2026-05-20 11:42:28 +0200

    f

---
+++ 3.3.9/script.js
@@ -0,0 +1,19 @@
+console.log("It's nice day")
+console.log('Moje jméno je "honza"')
+
+let username = "Tomáš"
+let message = `Moje jméno je ${username}` ;
+console.log(message);
+
+let a=5;
+let b=10
+console.log(`Soucet je ${a+b}`)
+
+function pozdrav(name) {
+    return `Hello, ${name}!`;
+}
+console.log(pozdrav("Honza"))
+
+let popis = "Toto je text kter se rozprostira pres nekolik radku - a to je cool"
+console.log(popis)
+
